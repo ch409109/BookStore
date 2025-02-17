629d4447a9c5c7f4d23d7c208bb16e139e969499
conght <conghthe172673@fpt.edu.vn>
2025-02-17 15:55:37 +0700

    Add project files.

---
+++ Core/Entities/Book.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Book : BaseEntity
+    {
+        public required string Title { get; set; }
+        public required string Description { get; set; }
+        public int Price { get; set; }
+        public int QuantityInStock { get; set; }
+        public required string ISBN { get; set; }
+        public required string Publisher { get; set; }
+        public int PublicationYear { get; set; }
+        public required string Language { get; set; }
+        public int PageCount { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,20 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext(DbContextOptions options) : DbContext(options)
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
